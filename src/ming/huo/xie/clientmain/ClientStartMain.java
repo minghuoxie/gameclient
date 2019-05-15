50da6a6fc6974de0aefc64be7001d05957cc8182
minghuoxie <2300671708@qq.com>
2019-05-15 16:47:17 +0800

    commit,第一次提交客户端代码。。。。

---
+++ src/ming/huo/xie/clientmain/ClientStartMain.java
@@ -0,0 +1,11 @@
+package ming.huo.xie.clientmain;
+
+public class ClientStartMain {
+    public static void main(String[] args){
+        /**
+         * 客户端主入口
+         *
+         * **/
+        System.out.println("---------哈哈哈哈");
+    }
+}
